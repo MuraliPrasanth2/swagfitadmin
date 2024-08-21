2d9d46a9c5c3ac0f3ae082fad801d7fba4d396b1
Murali Prasanth <mprasanth18@gmail.com>
2024-08-21 15:24:44 +0530

    added form question data in seperate file

---
+++ src/Data/remainingDetailsArrays.js
@@ -0,0 +1,305 @@
+// groupFitness
+// personalDanceTraining
+// personalFitness
+// psysioFitness
+export const groupFitnessRemainingDetailsArray = [
+    { dbName: "occupation", questionText: "Occupation" },
+    { dbName: "address", questionText: "Your location(city, area name)." },
+    { dbName: "timing", questionText: "Select your preferred slot" },
+    {
+        dbName: "fitnessProgramNames",
+        questionText:
+            "Have you participated in any fitness programs before? If so, which ones",
+    },
+    { dbName: "fitnessGoals", questionText: "Primary fitness goals?" },
+    {
+        dbName: "priorFitnessExperience",
+        questionText:
+            "Do you have any prior experience with CrossFit or high-intensity workouts?",
+    },
+    {
+        dbName: "medicalConditions",
+        questionText: "Do you have any existing medical conditions or injuries?",
+    },
+    {
+        dbName: "basicEquipment",
+        questionText:
+            "Do you have access to basic workout equipment (e.g., dumbbells, stepper, yoga mat)?",
+    },
+];
+
+export const personalFitnessRemainingDetailsArray = [
+    { dbName: "occupation", questionText: "Occupation" },
+    { dbName: "address", questionText: "Your location(city, area name)." },
+    {
+        dbName: "currentFitnessLevel",
+        questionText: "How would you rate your current fitness level?",
+    },
+    {
+        dbName: "regularExerciseRoutine",
+        questionText: "Do you currently have a regular exercise routine?",
+    },
+    {
+        dbName: "regularExerciseRoutineDescribe",
+        questionText: "If yes, please describe your current exercise routine:",
+    },
+    {
+        dbName: "fitnessGoals",
+        questionText: "What are your primary fitness goals?",
+    },
+    {
+        dbName: "specifitAreaToFocuesFitnessGoals",
+        questionText:
+            "Do you have any specific areas you want to focus on? (e.g., core strength, cardiovascular health, upper body strength, etc.)",
+    },
+    {
+        dbName: "upcomingEventOrMilestone",
+        questionText:
+            "Are there any upcoming events or milestones you are training for? (e.g., sports competition, athletic)",
+    },
+    {
+        dbName: "preexistingMedicalCondition",
+        questionText:
+            "Do you have any pre-existing medical conditions or injuries?",
+    },
+    {
+        dbName: "preexistingMedicalConditionDetails",
+        questionText: "If yes, please provide details:",
+    },
+    {
+        dbName: "takingMedication",
+        questionText:
+            "Are you currently taking any medications that may affect your ability to exercise?",
+    },
+    {
+        dbName: "takingMedicationDetails",
+        questionText: "If yes, please provide details:",
+    },
+    {
+        dbName: "preferedTrainingTime",
+        questionText: "What is your preferred training time?",
+    },
+    {
+        dbName: "daysAvailableForTraining",
+        questionText:
+            "How many days per week are you available for training sessions?",
+    },
+    {
+        dbName: "preferedTrainingType",
+        questionText: "Do you have any preferences for the type of training?",
+    },
+    {
+        dbName: "preferedSessionType",
+        questionText:
+            "Do you prefer virtual sessions, in-person sessions, or a mix of both?",
+    },
+    {
+        dbName: "workedWithPersonalTrainer",
+        questionText: "Have you worked with a personal trainer before?",
+    },
+    {
+        dbName: "workedWithPersonalTrainerExperience",
+        questionText: "If yes, please share your experience:",
+    },
+    {
+        dbName: "informationToTailorTrainingProgram",
+        questionText:
+            "Is there anything else we should know to help tailor your training program?",
+    },
+    {
+        dbName: "termsAndConditions",
+        questionText:
+            "I agree to the terms and conditions of the personal training program.",
+    },
+    {
+        dbName: "consentForStoringPersonalInformation",
+        questionText:
+            "I consent to having my personal information stored for the purpose of receiving training services.",
+    },
+];
+
+export const personalPhysioFitnessRemainigDetailsArray = [
+    {
+        dbName: "currentMedicalConditions",
+        questionText:
+            "Do you have any current medical conditions? (e.g., diabetes, hypertension, asthma, etc.)",
+    },
+    {
+        dbName: "pastInjuries",
+        questionText:
+            "Have you had any past injuries? If yes, please describe them.",
+    },
+    {
+        dbName: "surgeries",
+        questionText: "Have you had any surgeries? If yes, please describe them.",
+    },
+    {
+        dbName: "takingMedication",
+        questionText: "Are you currently taking any medication?",
+    },
+    {
+        dbName: "medications",
+        questionText: "If yes, please list the medications.",
+    },
+    {
+        dbName: "knownAllergies",
+        questionText: "Do you have any known allergies? If yes, please list them.",
+    },
+    {
+        dbName: "previousPhysicalTherapy",
+        questionText:
+            "Have you ever undergone physical therapy or rehabilitation? If yes, please describe the reason and outcome.",
+    },
+    {
+        dbName: "areasOfPain",
+        questionText:
+            "Are you experiencing any areas of pain or discomfort? If yes, please describe them.",
+    },
+    {
+        dbName: "currentPainLevel",
+        questionText:
+            "On a scale of 1 to 10, how would you rate your current pain level?",
+    },
+    {
+        dbName: "painAffectingDailyActivities",
+        questionText:
+            "Is your pain affecting your daily activities? If yes, please describe how.",
+    },
+    {
+        dbName: "exacerbatingActivities",
+        questionText:
+            "Are there any activities that exacerbate your pain? If yes, please describe them.",
+    },
+    {
+        dbName: "priorFitnessExperience",
+        questionText:
+            "Do you have any prior fitness experience? If yes, please describe it.",
+    },
+    {
+        dbName: "specificRehabExercises",
+        questionText:
+            "Are you currently doing any specific rehabilitation exercises? If yes, please describe them.",
+    },
+    {
+        dbName: "currentFitnessGoals",
+        questionText:
+            "What are your current fitness goals? (e.g., weight loss, muscle gain, improve endurance, etc.)",
+    },
+    {
+        dbName: "exerciseFrequency",
+        questionText: "How many days per week do you currently exercise?",
+    },
+    {
+        dbName: "typesOfExercises",
+        questionText:
+            "What types of exercises do you typically do? (e.g., running, weightlifting, yoga, etc.)",
+    },
+    {
+        dbName: "specificAreasToRehabilitate",
+        questionText:
+            "Are there any specific areas of your body you would like to rehabilitate or strengthen? If yes, please describe them.",
+    },
+    {
+        dbName: "challengingExercises",
+        questionText:
+            "Are there any exercises that you find particularly challenging or uncomfortable? If yes, please describe them.",
+    },
+    {
+        dbName: "trainingPreferences",
+        questionText:
+            "Do you have any preferences for your training sessions? (e.g., type of training, location, etc.)",
+    },
+    {
+        dbName: "preferredTrainingDaysAndTimes",
+        questionText:
+            "What are your preferred days and times for training sessions?",
+    },
+    { dbName: "occupation", questionText: "What is your occupation?" },
+    {
+        dbName: "occupationInvolvesPhysicalLabor",
+        questionText: "Does your occupation involve physical labor?",
+    },
+    {
+        dbName: "hobbiesAndActivities",
+        questionText: "What are your hobbies and recreational activities?",
+    },
+    {
+        dbName: "additionalInformation",
+        questionText:
+            "Is there any additional information you would like to provide?",
+    },
+    {
+        dbName: "concernsOrQuestions",
+        questionText: "Do you have any concerns or questions about the program?",
+    },
+];
+
+export const personalDanceFitnessRemainingDetailsArray = [
+    {
+        dbName: "preferredContactMethod",
+        questionText: "Preferred Contact Method",
+    },
+    {
+        dbName: "previousDanceTraining",
+        questionText: "Have you had any previous dance training?",
+    },
+    {
+        dbName: "danceStyles",
+        questionText: "If yes, what styles of dance have you practiced?",
+    },
+    { dbName: "yearsDancing", questionText: "How long have you been dancing?" },
+    {
+        dbName: "currentFitnessLevel",
+        questionText: "How would you describe your current fitness level?",
+    },
+    {
+        dbName: "experienceWithDanceFitness",
+        questionText: "Do you have any experience with dance fitness programs?",
+    },
+    { dbName: "fitnessGoals", questionText: "What are your fitness goals?" },
+    {
+        dbName: "danceStylesInterested",
+        questionText: "What styles of dance are you interested in?",
+    },
+    {
+        dbName: "specificDanceGoals",
+        questionText: "Do you have any specific dance goals you’d like to achieve?",
+    },
+    {
+        dbName: "preferredTrainingFrequency",
+        questionText: "How often would you like to train?",
+    },
+    {
+        dbName: "medicalConditions",
+        questionText: "Do you have any medical conditions we should be aware of?",
+    },
+    {
+        dbName: "medicalConditionDetails",
+        questionText: "If yes, please provide details",
+    },
+    {
+        dbName: "takingMedication",
+        questionText: "Are you currently taking any medication?",
+    },
+    {
+        dbName: "medicationDetails",
+        questionText: "If yes, please provide details",
+    },
+    { dbName: "allergies", questionText: "Do you have any allergies?" },
+    { dbName: "allergyDetails", questionText: "If yes, please provide details" },
+    {
+        dbName: "preferredTrainingDays",
+        questionText: "What days of the week are you available for training?",
+    },
+    {
+        dbName: "preferredTrainingTimes",
+        questionText: "What times of day are you available for training?",
+    },
+];
+
+export const remainingDetailsArray = {
+    groupFitness: groupFitnessRemainingDetailsArray,
+    personalDanceTraining: personalDanceFitnessRemainingDetailsArray,
+    personalFitness: personalFitnessRemainingDetailsArray,
+    psysioFitness: personalPhysioFitnessRemainigDetailsArray,
+};
